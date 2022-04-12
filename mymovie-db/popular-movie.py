8ce41b7d3c075dd55d623f4577ff8eb7646af832
imkzuma <98073935+imkzuma@users.noreply.github.com>
2022-04-13 01:09:33 +0800

    Create popular-movie.py

---
+++ mymovie-db/popular-movie.py
@@ -0,0 +1,22 @@
+#Popular movie Graph based on popularity
+
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+url = "mymoviedb.csv"
+data = pd.read_csv(url , lineterminator = "\n")
+
+Name = []
+Popular = []
+
+for index , row in data.iterrows():
+    Name.append(row["Title"])
+    Popular.append(row["Popularity"])
+
+    if index == 4: break
+
+plt.bar(Name , Popular)
+plt.title("Top 5 Popular Movie This Year")
+
+plt.ylabel("Popularity")
+plt.show()
