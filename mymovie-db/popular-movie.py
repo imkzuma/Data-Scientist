6ff7949a5a7f3dce1c4d42072db895e3c50d0c11
imkzuma <98073935+imkzuma@users.noreply.github.com>
2022-04-13 12:24:17 +0800

    Update popular-movie.py

--- mymovie-db/popular-movie.py
+++ mymovie-db/popular-movie.py
@@ -6,6 +6,21 @@ import matplotlib.pyplot as plt
 url = "https://raw.githubusercontent.com/imkzuma/Data-Scientist/main/mymovie-db/mymoviedb.csv"
 data = pd.read_csv(url , lineterminator = "\n")
 
+def ShowGraph(Name , Popular):
+    for index in range(5):
+        plt.bar(Name[index] , Popular[index] , label = str(index + 1) + " " + Name[index] + " = " + str(Popular[index])) 
+
+    plt.title("Top 5 Popular Movie This Year")
+    plt.legend()
+
+    plt.ylabel("Popularity")
+    plt.show()
+
+data = data.sort_values(["Popularity"] , ascending = False)
+data = data.filter(items = ["Title" , "Popularity"])
+
+print(data.head(5))
+
 Name = []
 Popular = []
 
@@ -15,8 +30,4 @@ for index , row in data.iterrows():
 
     if index == 4: break
 
-plt.bar(Name , Popular)
-plt.title("Top 5 Popular Movie This Year")
-
-plt.ylabel("Popularity")
-plt.show()
+ShowGraph(Name , Popular)
